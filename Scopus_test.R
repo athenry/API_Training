f79200500a2766b2f27ade3ed4de487422f0fec3
Alison Henry <alisontaylorhenry@gmail.com>
2019-11-20 13:17:23 -0700

    saving in case of crash!

---
+++ Scopus_test.R
@@ -0,0 +1,26 @@
+## testing  access to the Scopus API
+
+## Step one: install needed packages
+library(tidyverse)
+library(bibliometrix)
+library(rscopus)
+
+## Step two: Set your API key
+set_api_key("875146d79ec70b39b9fcbc099b07e321")
+
+##test that it exists. If so, this function will return TRUE.
+have_api_key()
+
+## Step three: try a search to see if your access works
+UA_info <- get_affiliation_info(affil_name = "University of Alberta")
+
+## Read in our list of authors
+mecenames <- read.csv("mecenames.csv", header = TRUE)
+
+## add in a column for the affiliatiomn
+mecenames$affiliation <- "alberta"
+
+## Get the Scopus IDs of these authors
+## for this function we need the API key as a variable
+api_key = get_api_key()
+mece_authors <- idByAuthor(mecenames, api_key)
